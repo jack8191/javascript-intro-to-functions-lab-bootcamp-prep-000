92384c472b5908726625c3ae024604b1c9286a8d
jack eichner <jack8191@gmail.com>
2018-05-28 10:24:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ console.log(string.toLowerCase())
 }
 
 
-function sayHitoGrandma(string) {
+function sayHiToGrandma(string) {
  if (string === 'I love you, Grandma')
  return 'I love you, too.'
  
